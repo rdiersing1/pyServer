0c20b4ebc09c68d1c6b675ba8c534caec3e46f17
Robert Diersing <rdiersin@usc.edu>
2019-01-02 16:41:38 -0800

    made accessable from remote computer on the network

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from flask import render_template
 from flask import request
 app = Flask(__name__)
 
+@app.run(host='0.0.0.0')
+
 @app.route('/')
 def index():
     return render_template('index.html')
